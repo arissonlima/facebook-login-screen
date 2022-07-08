d94a9138694c52599582e0cc0da0e694f0351fde
Arisson Lima <arissonlima@gmail.com>
2022-07-08 19:44:59 -0300

    First commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        blue: "#1B77F2",
+        backg: "#F0F2F5",
+        green: "#36A420",
+      },
+      spacing: {
+        300: "300px",
+        380: "380px",
+        580: "580px",
+      },
+    },
+  },
+  plugins: [],
+};
